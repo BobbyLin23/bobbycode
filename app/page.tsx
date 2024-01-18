0f465e6f05019864a4ae75518b4ce21507290e7f
BobbyLin23 <linzhangsheng23@gmail.com>
2024-01-18 10:19:20 +0800

    feat: add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main>
       <div className="p-2">
         <Button>Click</Button>
+        <ModeToggle />
       </div>
     </main>
   )
